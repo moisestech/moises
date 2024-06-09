6b8d3acea0829b96b63fb098eb544854c18bca35
moisestech <m@moises.tech>
2024-06-09 09:52:31 -0400

    update copy continue testing deployment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,19 +2,12 @@ export default async function About() {
     return (
         <div className="flex flex-col gap-10">
             <h1 className="text-5xl">About</h1>
-            <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores mollitia minus unde ipsa, sed aperiam temporibus commodi? Quae, quasi. Delectus, recusandae quaerat nihil quasi quisquam libero blanditiis enim doloremque consequuntur?
-                Ad atque doloremque, reprehenderit in esse at aut eum perferendis nisi tempore, obcaecati, fugiat temporibus molestias similique accusantium distinctio maiores nesciunt voluptate soluta aliquid ducimus nostrum. Molestiae fuga voluptatem iure?
+            <h2>Moises Sanabria</h2>
+            <p className="w-1/2">
+                Venezuelan-born, Miami-based Moises Sanabria is an interdisciplinary artist whose work extends dialectics of machine philosophy alongside trends in memetics and branding through the context of networked social-media life. Moises' work is philosophical and political, joining academic aesthetics with internet meme cultures. His practice is deeply entangled with digital newness, live-streaming, new media, machine learning, and installation to connect science advancement with art further.
             </p>
             <p className="w-1/2">
-                Tempora doloribus molestiae repellat pariatur nisi, harum officiis tempore esse rem ex est animi itaque iste a neque atque alias ipsam labore. Qui sed aspernatur perspiciatis dolores, eius repellat atque.
-                Soluta laborum earum dignissimos facilis incidunt ducimus blanditiis, explicabo totam ipsum hic itaque atque illum quia, tempore animi! Ex eaque facilis incidunt blanditiis voluptas fuga dicta ab eveniet assumenda veniam!
-            </p>
-            <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores mollitia minus unde ipsa, sed aperiam temporibus commodi? Quae, quasi. Delectus, recusandae quaerat nihil quasi quisquam libero blanditiis enim doloremque consequuntur?
-                Ad atque doloremque, reprehenderit in esse at aut eum perferendis nisi tempore, obcaecati, fugiat temporibus molestias similique accusantium distinctio maiores nesciunt voluptate soluta aliquid ducimus nostrum. Molestiae fuga voluptatem iure?
-            </p>
-            <p className="w-1/2">
-                Tempora doloribus molestiae repellat pariatur nisi, harum officiis tempore esse rem ex est animi itaque iste a neque atque alias ipsam labore. Qui sed aspernatur perspiciatis dolores, eius repellat atque.
-                Soluta laborum earum dignissimos facilis incidunt ducimus blanditiis, explicabo totam ipsum hic itaque atque illum quia, tempore animi! Ex eaque facilis incidunt blanditiis voluptas fuga dicta ab eveniet assumenda veniam!
+                Moises co-founded the new artificial intelligence media channel AI24. Previously, he was an active member of the digital art collective ART404 (Artnotfound) from 2011 to 2019, which he co-founded. Moises attended the School of Poetic Computation and the Cooper Union in New York. He has exhibited at Transmediale 2k+12, Hause Der Kulturen Der Welt (Berlin), Institute of Contemporary Art Miami, Bikini Wax Gallery (Mexico City), Kunsthalle Giessen (Berlin), and has participated in group shows internationally. In addition, he has been featured in Rhizome and The Miami Rail.
             </p>
         </div>
     )
