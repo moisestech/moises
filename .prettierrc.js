0e2eb982ee05fe7daa42522302dd0a654dac8f8b
moisestech <m@moises.tech>
2024-06-09 14:53:13 -0400

    update metadata description in layout continue testing deployment

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// .prettierrc.js
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'auto',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
